031e2ea43f76177b8a6b772e1561ccec51aae49f
Soren_nielsen <soren.nielsen599@gmail.com>
2023-11-08 21:00:40 -0700

    added HW

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -20,7 +20,7 @@ public class SimpleQueue {
         queue.Enqueue(100);
         var value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found:
+        // Defect(s) Found: index was out of range line 79
 
         Console.WriteLine("------------");
 
@@ -38,7 +38,7 @@ public class SimpleQueue {
         Console.WriteLine(value);
         value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found: 
+        // Defect(s) Found: displaying the data backwards 
 
         Console.WriteLine("------------");
 
@@ -54,7 +54,7 @@ public class SimpleQueue {
         catch (IndexOutOfRangeException) {
             Console.WriteLine("I got the exception as expected.");
         }
-        // Defect(s) Found: 
+        // Defect(s) Found: none
     }
 
     private readonly List<int> _queue = new();
@@ -64,7 +64,7 @@ public class SimpleQueue {
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+        _queue.Add(value); // this should be add 
     }
 
     /// <summary>
@@ -76,8 +76,8 @@ public class SimpleQueue {
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0];  //index was out of range this should start at 0
+        _queue.RemoveAt(0);
         return value;
     }
 }
